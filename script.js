8cfeb166c0ed91ef1db78ed6a73becf60070babd
+ <+>
2021-09-28 14:01:23 -0400

    starting text content and style

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
+h2.textContent = "and more...";
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "And more";
+document.querySelector("body").appendChild("h3");
+
