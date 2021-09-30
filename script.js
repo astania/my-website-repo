00ac28477aceefdb771b38a2f148f9d05b91b15b
+ <+>
2021-09-30 12:37:28 -0400

    updated with fortune message

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-const h2 = document.createElement("h2");
-h2.textContent = "and more...";
-document.querySelector("body").appendChild(h2);
 
-const h3 = document.createElement("h3");
-h3.textContent = "And more";
-document.querySelector("body").appendChild("h3");
 
